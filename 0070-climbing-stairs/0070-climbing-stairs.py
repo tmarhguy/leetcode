553fd3c21cab5dffc3beee7b6ddffe9485cb5bb3
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-03-21 22:39:20 -0400

    Time: 15 ms (1%), Space: 11.5 MB (100%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def __init__(self):
+        self.memo = {}
+        
+    def climbStairs(self, n):
+        if n in self.memo:
+            return self.memo[n]
+        
+        if n == 1:
+            self.memo[n] = 1
+        elif n == 2:
+            self.memo[n] = 2
+        else:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        return self.memo[n]
