12bef54379ed3c551f389883b2c0da1a62983c13
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-03-21 23:02:33 -0400

    Time: 0 ms (100%), Space: 17.6 MB (98%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.py
+++ 2556-convert-the-temperature/2556-convert-the-temperature.py
@@ -1,13 +1,4 @@
-class Solution(object):
-    def convertTemperature(self, celsius):
-        #conversion into Kelvin
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
         return [celsius + 273.15, celsius * 1.8 + 32.0]
-        
-        
-        
-        
-        """
-        :type celsius: float
-        :rtype: List[float]
-        """
         
