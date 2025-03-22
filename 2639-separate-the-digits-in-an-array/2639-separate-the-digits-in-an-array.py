c1ebd5a6f682a4eafa821bae3f561f9675bcde8c
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-03-21 23:09:57 -0400

    Time: 6 ms (26.09%), Space: 18.2 MB (12.48%) - LeetHub

--- 2639-separate-the-digits-in-an-array/2639-separate-the-digits-in-an-array.py
+++ 2639-separate-the-digits-in-an-array/2639-separate-the-digits-in-an-array.py
@@ -1,4 +1,15 @@
-class Solution(object):
-    def separateDigits(self, nums):
-        nums = ''.join(map(str,nums))
-        return [int(digit) for digit in nums]
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        result = []
+
+        for num in nums:
+            if len(str(num)) > 1:
+                temp = list(str(num))
+                temp = [int(num) for num in temp]
+                result += temp
+
+            else:
+                result.append(num)
+
+        return result
+        
