5b21964c4d2108707a715808464e4c8b248e6378
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-03-21 22:39:39 -0400

    Time: 8 ms (3.34%), Space: 11.5 MB (100%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        master = [[1],[1,1]]
+        numRows = rowIndex+1
+        while len(master) < numRows:
+
+            prev = master[-1]
+            len_of_list = len(prev)
+            temp_list = [1]
+
+            for i in range(len_of_list-1):
+                temp_list.append(prev[i] + prev[i+1])
+            temp_list.append(1)
+            master.append(temp_list)
+
+
+        return master[rowIndex]
