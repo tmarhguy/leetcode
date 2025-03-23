dc4a63c9278389bda5a8374b88bec35a7660c1ae
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-03-23 10:06:42 -0400

    Time: 4 ms (27.10%) | Memory: 12.6 MB (20.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,19 @@
 class Solution(object):
     def isValid(self, s):
         brack = {')':'(','}':'{',']':'[' }
-        array = []
-        for c in s:
-            if c in brack:
-                if array and array[-1] == brack[c]:
-                    array.pop()
-            
+        s_stack = []
+
+        if len(s) % 2 == 1: return False
+
+        for char in s:
+            if char not in brack.keys():
+                s_stack.append(char)
+            else:
+                if len(s_stack) > 0:
+                    if brack[char] == s_stack[-1]:
+                        s_stack.pop()
+                    else:
+                        return False
                 else:
                     return False
-            else:
-                array.append(c)
-            
-        return True if not array else False
+        return True if len(s_stack) == 0 else False
