b0b4a81ffd44f6ee80d494cd565b762e820625bb
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-03-27 00:20:41 -0400

    Time: 353 ms (23.56%), Space: 17.6 MB (89.25%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dict_ans = {0:0,1:1}
-        if n in dict_ans:
-            return dict_ans[n]
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
         else:
-            answer = self.fib(n-1) + self.fib(n-2)
-            dict_ans[n] = answer
-            return answer
+            return self.fib(n-1) + self.fib(n-2)
         
