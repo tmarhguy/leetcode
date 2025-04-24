bf641bd880f4a8729720a8832fac82762b8b40ef
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-04-24 15:02:16 -0400

    Time: 0 ms (100%), Space: 17.7 MB (90.06%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -4,11 +4,11 @@ class Solution:
             return True
         n = bin(n)
 
-        if len(n) % 2 == 0:
+        if len(str(n)) % 2 == 0:
             return False
         else:
-            if n[2] == "1":
-                if all(bit == "0" for bit in n[3:]):
+            if str(n)[2] == "1":
+                if all(bit == "0" for bit in str(n)[3:]):
                     return True
         return False
         
