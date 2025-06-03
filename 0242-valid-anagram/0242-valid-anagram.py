f43d4e9905e1ef58e7fa5aa79ce16d0386f93e9f
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-06-02 21:09:54 -0400

    Time: 11 ms (72.66%), Space: 17.8 MB (86.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
         s_map = defaultdict(int)
 
-        for char in s:
-            s_map[char] += 1
-        
-        for char in t:
-            s_map[char] -= 1
-
+        for i in range(len(s)):
+            s_map[s[i]] += 1
+            s_map[t[i]] -= 1
+            
         return all(value == 0 for value in s_map.values())
         
