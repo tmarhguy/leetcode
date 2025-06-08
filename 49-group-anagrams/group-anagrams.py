37fe9174e767126357213acf6795b9a38d70abb9
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-06-08 19:48:34 -0400

    Time: 7 ms (98.08%) | Memory: 21.2 MB (44.17%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hash_set = defaultdict(list)
-        key = [0 for _ in range(26)]
+        # key = [0 for _ in range(26)]
 
         for word in strs:
+            """
             for char in word:
                 key[ord(char) - ord('a')] += 1
-            key = tuple(key)
+            """
+            key = tuple(sorted(word))
             hash_set[key].append(word)
-            key = [0 for _ in range(26)]
-            
+            # key = [0 for _ in range(26)]
+
         return list(hash_set.values())
 
         
