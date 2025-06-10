2aee7012573770fbe022a519e6e0a5f097352d0b
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-06-09 20:48:08 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (10.27%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,11 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+
         valid = {"(":")","{":"}","[":"]"}
         stack = []
 
         for char in s:
-            if char in valid.keys():
+            if char in valid:
                 stack.append(char)
             else:
                 if stack and char == valid[stack[-1]]:
