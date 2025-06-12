7a68b537d7d4a57cd9912b1274d408c7b6fa40cb
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-06-12 19:32:49 -0400

    Time: 0 ms (100%), Space: 17.8 MB (37.97%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,17 +1,15 @@
 class Solution:
-    def isHappy(self, n):
+    def isHappy(self, n: int) -> bool:
+        def happy(n):
+            answer = 0
+            for char in str(n):
+                answer += int(char) ** 2
+            return answer
         
-        if n == 7 or n == 10: return True
-        else:
-            while n > 10:
-                lst = []
-                n = str(n)
-                n = list(n)
-                n = [int(i) for i in n]
-                m = 0
-                for a in n:
-                    m += a**2
-                    n = m
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = happy(n)
 
-            if n == 1 or n == 7 or n == 10: return True
-            else: return False
+        return n == 1
+        
