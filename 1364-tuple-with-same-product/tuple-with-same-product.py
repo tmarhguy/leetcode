0444e8584b1ed520ef43827a7647120b066ea0e3
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-06-13 13:51:55 -0400

    Time: 332 ms (84.13%) | Memory: 46.6 MB (26.77%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.py
@@ -0,0 +1,15 @@
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        seen = defaultdict(int)
+        count = 0
+
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                prod = nums[i] * nums[j]
+                seen[prod] += 1
+
+        for value in seen.values():
+            if value >= 2:
+                count += math.comb(value,2)
+        return count * 8
+        
