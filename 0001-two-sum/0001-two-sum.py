79fcc1aaf3bc4e8490fbe58c4e0105d95ee2f292
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-06-12 21:23:40 -0400

    Time: 0 ms (100%), Space: 19.1 MB (10.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
+        # using hash set
         hash_nums = {}
 
-        for index, number in enumerate(nums):
-            complement = target - number
+        for i,num in enumerate(nums):
+            complement = target - num
             if complement not in hash_nums:
-                hash_nums[number] = index
+                hash_nums[num] = i
             else:
-                return [hash_nums[complement], index]
+                return [hash_nums[complement], i]
+
         
