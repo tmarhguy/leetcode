aeab1ad93eeef75850bcd5287f3fd82466886d58
Tyrone Marhguy <tyronemarhguy@gmail.com>
2025-06-23 01:20:26 -0400

    Time: 7 ms (32.21%) | Memory: 17.6 MB (97.48%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,30 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        x = l1
-        y = l2
-        x_sum = 0
-        y_sum = 0
-        i = 0
+        l1_sum, l2_sum, i = 0, 0, 0
 
-        while x or y:
-            if x:
-                x_sum += x.val * 10 ** i
-                x = x.next
-            if y:
-                y_sum += y.val * 10 ** i
-                y = y.next
+        while l1 or l2:
+            if l1:
+                l1_sum += l1.val * 10 ** i
+                l1 = l1.next
+            if l2:
+                l2_sum += l2.val * 10 ** i
+                l2 = l2.next
             i += 1
-        
-        answer = [int(value) for value in str(x_sum+y_sum)]
-        answer.reverse()
-        
+
+        answers = [int(value) for value in str(l1_sum + l2_sum)]
+        answers.reverse()
         dummy = ListNode()
         current = dummy
 
-        for num in answer:
+        for num in answers:
             current.next = ListNode(num)
             current = current.next
         return dummy.next
-
-        
